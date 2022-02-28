5e6ce6d2c408420a3b89ea920d011a6d613c83dc
Garrett <ga.landin89@gmail.com>
2022-02-27 19:47:16 -0600

    Messed around with possible while loop function and added some questions

--- scratch.py
+++ scratch.py
@@ -13,11 +13,6 @@ random_restaurant = random.choice(restaurants)
 random_travel = random.choice(travel_modes)
 random_entertainment = random.choice(entertainment_types)
 
-string_destination = "destination"
-string_restaurant = "restaurant"
-string_travel = "mode of transportation"
-string_entertainment = "form of entertainment"
-
 def moving_on():
     print("Great, I'm glad that's decided! Let's move on...")
 
@@ -25,21 +20,51 @@ def confirm_destination_selection():
     answer = input(f"We have selected {random_destination} as your destination. Does this sound good to you? Enter y/n: ")
     return answer
 
-def oh_sorry_destination():
-    answer = input(f"Oh, sorry you didnt like this choice. No worries, we can try something else! How about {random_destination}? Enter y or n: ")
+def confirm_travel_selection():
+    answer = input(f"We have selected {random_travel} as your mode of transportation. Does this sound good to you? Enter y/n: ")
     return answer
 
-destination_confirmed = confirm_destination_selection()
-travel_mode_confirmed = 
-confirmed = False
+def confirm_entertainment_selection():
+    answer = input(f"We have selected {random_entertainment} as your form of entertainment for the day. Does this sound good to you? Enter y/n: ")
+    return answer
 
-while confirmed is False:
-    if destination_confirmed == "y":
-        confirmed = True
-        moving_on()
-    else:
-        random_destination = random.choice(destinations)
-        destination_confirmed = input(f"Oh, sorry you didnt like this choice. No worries, we can try something else! How about {random_destination}? Enter y or n: ")
+def confirm_restaurant_selection():
+    answer = input(f"We have selected {random_restaurant} as your restaurant. Does this sound good to you? Enter y/n: ")
+    return answer
 
-print(random_destination)
+def finalize_trip():
+    answer = input("Would you like to finalize this trip? Enter y or n: ")
+    return answer
 
+def finalize_trip2():
+#    print("It looks like you may have made a typo... ")
+    answer = input("Would you like to finalize this trip? Enter y or n: ")
+    return answer
+
+#1. This function does too many things for what the project requirements specify
+
+#2. How would I call the value of variable random_destination later when confirming
+#   the trip if the value is scoped locally to this function?
+
+#3. How do I set the re roll of a new random specific element to a function if the 
+#   user decides at the end of this that they want to change one element if the trip?
+
+#4. Could I write this function so that line 11 is declared inside this function so that when the captured value is called to finalize
+#   the trip, it still holds the value that was declared within the function or no because the value would be getting called outside 
+#   the scope of the function?
+
+def destination_loop():
+    destination_confirmed = confirm_destination_selection()
+    confirmed = False
+    while confirmed is False:
+        if destination_confirmed == "y":
+            confirmed = True
+            moving_on()
+        else:
+            random_destination = random.choice(destinations) #the new value is now unable to be recalled for confirming trip because its local to this function
+            destination_confirmed = input(f"Oh, sorry you didn't like this choice. No worries, we can try something else! How about {random_destination}? Enter y or n: ")
+            
+
+destination_loop()
+
+print(random_destination) #prints something different than what was selected because its now outside the function
